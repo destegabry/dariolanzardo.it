6d2d00257894546d5c53b508fdef845f4bd3fc29
Gabriele Destefanis <g.destefanis@topsolution.it>
2017-07-24 12:21:09 +0200

    Removed dummy blog

--- blog/wp-content/themes/lanzardo/footer.php
+++
@@ -1,9 +0,0 @@
-		</div><!-- end body -->
-                </div><!--end page -->
-
-<div id="footer"><!--start footer -->
-	<p>&copy;2006 - Dario Lanzardo<br/></p>
-</div><!--end footer -->
-
-</body>
-</html>
